15d8a18b7f95e4589d79871e22a971126d9b674c
Yair Slonim <yairslo7@gmail.com>
2022-04-26 12:56:05 +0300

    login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,26 @@
 import React, {useState} from 'react'
+import Axios from "axios";
+import Cookies from "universal-cookie";
 
 function Login() {
 
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
+    const cookies = new Cookies();
    const login =() => {
-
+    Axios.post("http://localhost:3001/login", {
+        username,
+        password,
+      }).then((res) => {
+        const { firstName, lastName, username, token, userId } = res.data;
+        cookies.set("token", token);
+        cookies.set("userId", userId);
+        cookies.set("username", username);
+        cookies.set("firstName", firstName);
+        cookies.set("lastName", lastName);
+        setIsAuth(true);
+      });
    }
 
     return ( 
